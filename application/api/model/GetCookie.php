24f1afd4823c0b7e6a33f72184a6aa4ca9618ead
0a9_1549863239_chunlai <0a9_1549863239_chunlai@git.cloud.tencent.com>
2018-12-18 13:23:21 +0800

    获取cookie

--- application/api/model/GetCookie.php
+++ application/api/model/GetCookie.php
@@ -23,25 +23,9 @@ class GetCookie
         curl_setopt($ch,CURLOPT_TIMEOUT,10);
         $content = curl_exec($ch);
         curl_close($ch);
-//        var_dump(getallheaders());return;
-//        var_dump(get_headers("http://172.16.2.39/jsxsd/", 1));return;
-//        echo $content;return;
-        //TODO:异常以后写
-//        try{
-//            // 解析HTTP数据流
         list($header, $body) = explode("\r\n\r\n", $content);
-//        }catch (Exception $e){
-//            throw new ServerException();
-//        };
-        // 解析COOKIE
         preg_match_all("/(set\-cookie: )([^\r\n]*)(;)/i", $header, $matches);
-        // 后面用CURL提交的时候可以直接使用
-        //TODO:异常以后写
-//        try{
         $cookie = $matches[2][0] . ";" . $matches[2][1] . ";";
-//        }catch (Exception $e){
-//            throw new NetworkingException();
-//        };
         return $cookie;
     }
 }
